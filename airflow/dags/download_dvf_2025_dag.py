524ef637b3de089d21527a766bc902fbc5b62afb
Beline21 <colinebesson@hotmail.fr>
2026-03-11 16:49:35 +0100

    Download Dags modified better

--- airflow/dags/download_dvf_2025_dag.py
+++ airflow/dags/download_dvf_2025_dag.py
@@ -28,8 +28,10 @@ def dvf_2025_dag():
     @task()
     def download_dvf():
         # URL du fichier DVF 2025 (exemple, à remplacer par l’URL exacte)
-        url = "https://www.data.gouv.fr/api/1/"
-	"datasets/r/4d741143-8331-4b59-95c2-3b24a7bdbe3c"
+        url = (
+            "https://www.data.gouv.fr/api/1/"
+	    "datasets/r/4d741143-8331-4b59-95c2-3b24a7bdbe3c"
+        )
         filename = os.path.join(DATA_DIR, "dvf_2025.csv")
 
         # Crée le dossier si n’existe pas
