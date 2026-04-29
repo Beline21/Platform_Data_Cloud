0782eb58e6b32b4e0d960b5bbc109dd65976c2ea
Beline21 <colinebesson@hotmail.fr>
2026-04-29 16:37:42 +0200

    new DAG E2E fixed

--- airflow/dags/e2e_dag.py
+++ airflow/dags/e2e_dag.py
@@ -8,7 +8,7 @@ from download_open_meteo_dag import fetch_weather as fetch_meteo
 from download_open_meteo_dag import load_to_bronze as load_meteo_to_bronze
 from download_dvf_2025_dag import download_and_extract_dvf as fetch_dvf
 from download_dvf_2025_dag import load_to_bronze as load_dvf_to_bronze
-from load_bronze import load_meteo_to_bronze, load_dvf_to_bronze
+# from load_bronze import load_meteo_to_bronze, load_dvf_to_bronze
 
 DBT_PROJECT_DIR = "/opt/airflow/dbt"
 
