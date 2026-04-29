9f97341404cfdacf80aaab0f5210d49e2b6c7ed5
Beline21 <colinebesson@hotmail.fr>
2026-04-29 17:07:18 +0200

    new DAG E2E fixed ??

--- airflow/dags/e2e_dag.py
+++ airflow/dags/e2e_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+from airflow.models.baseoperator import chain
 
 # Importer vos fonctions existantes
 from download_open_meteo_dag import fetch_weather as fetch_meteo
@@ -25,33 +26,33 @@ with DAG(
 ) as dag:
 
     extract_meteo = PythonOperator(
-        task_id="extract_meteo",
+        task_id="e2e_extract_meteo",
         python_callable=fetch_meteo,
     )
     extract_dvf = PythonOperator(
-        task_id="extract_dvf",
+        task_id="e2e_extract_dvf",
         python_callable=fetch_dvf,
     )
     load_meteo = PythonOperator(
-        task_id="load_meteo_bronze",
+        task_id="e2e_load_meteo_bronze",
         python_callable=load_meteo_to_bronze,
     )
     load_dvf = PythonOperator(
-        task_id="load_dvf_bronze",
+        task_id="e2e_load_dvf_bronze",
         python_callable=load_dvf_to_bronze,
     )
     run_dbt = BashOperator(
-        task_id="run_dbt",
+        task_id="e2e_run_dbt",
         bash_command=f"cd {DBT_PROJECT_DIR} && dbt run",
         cwd=DBT_PROJECT_DIR,
     )
     dbt_test = BashOperator(
-        task_id="dbt_test",
+        task_id="e2e_dbt_test",
         bash_command=f"cd {DBT_PROJECT_DIR} && dbt test",
         cwd=DBT_PROJECT_DIR,
     )
 
-    (
+    chain(
         [extract_meteo, extract_dvf]
         >> [load_meteo, load_dvf]
         >> run_dbt
