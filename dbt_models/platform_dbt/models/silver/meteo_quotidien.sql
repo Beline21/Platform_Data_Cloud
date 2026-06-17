55529c798392018697b1fbfeb5bfdd5fe12542d2
Beline21 <colinebesson@hotmail.fr>
2026-06-17 22:02:37 +0200

    meteo silver modified

--- dbt_models/platform_dbt/models/silver/meteo_quotidien.sql
+++ dbt_models/platform_dbt/models/silver/meteo_quotidien.sql
@@ -5,13 +5,38 @@
   )
 }}
 
-select
-    row_number() over () as cle_primaire,
-    TO_TIMESTAMP(time, 'YYYY-MM-DD"T"HH24:MI') AS time,
-    COALESCE(temperature_2m::FLOAT, 0) AS temperature_2m,
-    COALESCE(latitude::FLOAT, 0) AS latitude,
-    COALESCE(longitude::FLOAT, 0) AS longitude,
-    COALESCE(elevation::FLOAT, 0) AS elevation,
-    COALESCE(generationtime_ms::FLOAT, 0) AS generationtime_ms,
-    COALESCE(timezone::TEXT, '') AS timezone
-from {{ source('bronze', 'meteo_quotidien') }}
+WITH source AS (
+
+    SELECT
+        latitude,
+        longitude,
+        elevation,
+        generationtime_ms,
+        timezone,
+        hourly
+    FROM {{ source('bronze', 'meteo_quotidien') }}
+
+),
+
+meteo AS (
+
+    SELECT
+        ROW_NUMBER() OVER () AS cle_primaire,
+
+        hourly.value:time::TIMESTAMP AS time,
+
+        hourly.value:temperature_2m::FLOAT AS temperature_2m,
+
+        latitude::FLOAT AS latitude,
+        longitude::FLOAT AS longitude,
+        elevation::FLOAT AS elevation,
+        generationtime_ms::FLOAT AS generationtime_ms,
+        timezone::TEXT AS timezone
+
+    FROM source,
+    LATERAL FLATTEN(input => hourly:time) hourly
+
+)
+
+SELECT *
+FROM meteo
